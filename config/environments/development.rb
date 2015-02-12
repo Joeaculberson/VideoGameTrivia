c4b055db6b93e41b5057e6fbc4611276f25bdd48
Joe Culberson <Joeaculberson@gmail.com>
2015-02-11 23:23:54 -0500

    Sends email upon sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  Rails.application.routes.default_url_options[:host] = 'https://limitless-lowlands-6859.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'limitless-lowlands-6859.herokuapp.com'
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'fakecapstone@gmail.com',
+      password:             'capstone123',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
