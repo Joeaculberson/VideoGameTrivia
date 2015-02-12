cbb09bcf9d453f2cd6600cec77fcf72b49c4360b
Joe Culberson <Joeaculberson@gmail.com>
2015-02-11 22:33:36 -0500

    testing something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   Rails.application.routes.default_url_options[:host] = '???'
+  config.serve_static_assets = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
