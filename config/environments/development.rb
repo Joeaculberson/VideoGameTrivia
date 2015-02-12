580531cc6329360cb0a4b791778e7e593aaaee34
Joe Culberson <Joeaculberson@gmail.com>
2015-02-12 00:43:16 -0500

    removed domain name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'gmail.com',
       user_name:            'fakecapstone@gmail.com',
       password:             'capstone123',
       authentication:       'plain',
