55f3314664a8a70a22cde69e55e5e9e01b6bf622
kweathi1 <kweathi1@my.westga.edu>
2015-02-19 14:05:48 -0500

    Fixed login redirect issue

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,6 @@ ActiveRecord::Schema.define(version: 20150219174322) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "provider"
-    t.string   "uid"
     t.string   "username"
   end
 
