4a54a1675054ad36667fed3854b372a312076c84
Joe Culberson <Joeaculberson@gmail.com>
2015-03-26 17:25:20 -0400

    Set up the view modal for statistics and achievements.

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20150320220141) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "provider"
+    t.string   "uid"
     t.string   "username"
     t.string   "image"
     t.string   "role"
