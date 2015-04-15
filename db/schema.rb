912e937a58569bb7a828bf5142b367208c085c37
kweathi1 <kweathi1@my.westga.edu>
2015-04-14 21:27:47 -0400

    Added category leaderboards

--- db/schema.rb
+++ db/schema.rb
@@ -144,8 +144,6 @@ ActiveRecord::Schema.define(version: 20150415002102) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "provider"
-    t.string   "uid"
     t.string   "username"
     t.string   "image"
     t.string   "role"
